cd619673d74e9f2a8f8c7e8195bda59f58dc3fff
Maks Pavlenko <maks@maks.xyz>
2018-05-04 14:04:41 +0300

    fix translations

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,26 @@ jQuery(function ($) {
 
 //Start of--------------------------------Votes & Views-------------------------
 jQuery(function ($) {
-    $('.js-views').each(function () {
-        $.ajax({
-            url: '/ajax/views/',
-            data: {
-                element_id: $(this).data('element-id'),
-                element_type: $(this).data('element-type'),
-                action: $(this).data('action'),
-            },
-            method: 'GET',
-            dataType: 'jsonp',
-            context: this,
-            success: function (data) {
-                if (data.views) {
-                    $(this).html(t.text('js_total_views') + "\n<span class='views-count'>" + data.views + "</span>");
-                    $(this).trigger('loaded');
-                }
-            }
-        });
-    });
+    /*TODO: update to real count? 
+     $('.js-views').each(function () {
+     $.ajax({
+     url: '/ajax/views/',
+     data: {
+     element_id: $(this).data('element-id'),
+     element_type: $(this).data('element-type'),
+     action: $(this).data('action'),
+     },
+     method: 'GET',
+     dataType: 'jsonp',
+     context: this,
+     success: function (data) {
+     if (data.views) {
+     $(this).html(t.text('js_total_views') + "\n<span class='views-count'>" + data.views + "</span>");
+     $(this).trigger('loaded');
+     }
+     }
+     });
+     });*/
 
     $('.js-votes').each(function () {
         $.ajax({
@@ -38,10 +39,10 @@ jQuery(function ($) {
             context: this,
             success: function (data) {
                 if (data) {
-                    var htmlCode = "<span class='votes-label'>" + t.text('js_total_votes') + "</span>\n<span class='votes-count'>" + data.votes + "</span>";
+                    var htmlCode = "<span class='votes-label'>" + t('Всего голосов:') + "</span>\n<span class='votes-count'>" + data.votes + "</span>";
 
                     if (data.can_vote) {
-                        htmlCode += '<div class="vote js-vote">' + t.text('js_total_votes_vote') + '</div>';
+                        htmlCode += '<div class="vote js-vote">' + t('Проголосовать') + '</div>';
                     }
 
                     $(this).html(htmlCode);
@@ -66,14 +67,14 @@ jQuery(function ($) {
             context: $votes,
             success: function (data) {
                 if (data) {
-                    var htmlCode = "<span class='votes-label'>" + t.text('js_total_votes') + "</span>\n<span class='votes-count'>" + data.votes + "</span>";
+                    var htmlCode = "<span class='votes-label'>" + t('Всего голосов:') + "</span>\n<span class='votes-count'>" + data.votes + "</span>";
 
                     if (data.can_vote) {
-                        htmlCode += '<div class="vote js-vote">' + t.text('js_total_votes_vote') + '</div>';
+                        htmlCode += '<div class="vote js-vote">' + t('Проголосовать') + '</div>';
                     }
 
                     if (data.error === 'authorization_required') {
-                        htmlCode += '<div class="vote">' + t.text('js_authorization_required') + '</div>';
+                        htmlCode += '<div class="vote">' + t('Необходимо авторизоваться') + '</div>';
                     }
 
                     $(this).html(htmlCode);
